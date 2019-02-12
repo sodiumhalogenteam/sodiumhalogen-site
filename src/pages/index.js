f2b6fb12350f9c6d527138bbf588e10ad044d555
Derrick Smith <derricksmith01@gmail.com>
2019-02-12 15:01:48 -0600

    💅  STYLE: added particle.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import lozad from 'lozad'
+import Particles from 'react-particles-js'
 
 //components
 import Layout from '../components/layout'
@@ -101,9 +102,81 @@ class IndexPage extends React.Component {
   render() {
     return (
       <Styles>
+        <Particles
+          style={{
+            position: 'absolute',
+            top: 0,
+          }}
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: { value: '#000' },
+              shape: {
+                type: 'circle',
+                stroke: {
+                  width: 0,
+                  color: '#000000',
+                },
+                polygon: { nb_sides: 5 },
+                image: {
+                  src: 'img/github.svg',
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 0.4,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 1,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out',
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
         <Layout>
           <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-          <div id="particles-js" />
           <div className="team-gifs">
             <div className="hover-gif">
               <figure className="hover-gif__img">
