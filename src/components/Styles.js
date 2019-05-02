a4c361084f30212ccc1b1905c342f81261c82c30
Derrick Smith <derricksmith01@gmail.com>
2019-05-02 15:00:53 -0500

    💅  STYLE: fix hamburger hover style

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -6,6 +6,34 @@ import {
   serifFont,
 } from './Styles.mixins'
 
+function LightenDarkenColor(col, amt) {
+  var usePound = false
+
+  if (col[0] == '#') {
+    col = col.slice(1)
+    usePound = true
+  }
+
+  var num = parseInt(col, 16)
+
+  var r = (num >> 16) + amt
+
+  if (r > 255) r = 255
+  else if (r < 0) r = 0
+
+  var b = ((num >> 8) & 0x00ff) + amt
+
+  if (b > 255) b = 255
+  else if (b < 0) b = 0
+
+  var g = (num & 0x0000ff) + amt
+
+  if (g > 255) g = 255
+  else if (g < 0) g = 0
+
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16)
+}
+
 const palette = {
   themeprimary: '#8d980b',
   graybase: '#22292e',
@@ -567,7 +595,7 @@ const Styles = styled.div`
   }
 
   /*/////////
-  // Menu
+  // Burger Menu - yumm
   /////////*/
   /* Position and sizing of burger button */
   .bm-burger-button {
@@ -585,7 +613,8 @@ const Styles = styled.div`
 
   /* Color/shape of burger icon bars on hover*/
   .bm-burger-bars-hover {
-    color: red;
+    background: ${LightenDarkenColor(palette.themeprimary, -20)};
+    opacity: 1 !important;
   }
 
   /* Position and sizing of clickable cross button */
