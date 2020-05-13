9363c5ee7490f58c835141e5b619110f51c367df
Brantley English <sh-eva@brantleys-mbp.lan>
2020-05-13 11:07:47 -0500

    🐛  BUG: updates script to 50 mill before redirect to app store

--- static/brainstorm/scripts.js
+++ static/brainstorm/scripts.js
@@ -36,13 +36,21 @@ const deviceOS = getMobileOperatingSystem()
 
 if (deviceOS === 'Andriod' || deviceOS === 'iOS') {
   const prompt = getQueryVariable('prompt')
-  const now = new Date().valueOf()
-  setTimeout(() => {
-    if (new Date().valueOf() - now > 500) return
-    window.location =
-      deviceOS === 'iOS'
-        ? 'https://apps.apple.com/us/app/brain-storm/id1508034751?ls=1'
-        : 'https://play.google.com/store/apps/details?id=com.shbrainstorm'
-  }, 25)
+
+  if (deviceOS === 'Andriod') {
+    const now = new Date().valueOf()
+    setTimeout(function () {
+      if (new Date().valueOf() - now > 100) return
+      window.location =
+        'https://play.google.com/store/apps/details?id=com.shbrainstorm'
+    }, 50)
+  } else {
+    const now = new Date().valueOf()
+    setTimeout(function () {
+      if (new Date().valueOf() - now > 100) return
+      window.location =
+        'https://apps.apple.com/us/app/brain-storm/id1508034751?ls=1'
+    }, 50)
+  }
   window.location = prompt ? `brainstorm://${prompt}` : 'brainstorm://'
 }
