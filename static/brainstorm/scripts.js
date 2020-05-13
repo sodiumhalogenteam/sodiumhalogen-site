e6e58509451e26328cb63194cdf14ee1941cc1fa
Brantley English <sh-eva@brantleys-mbp.lan>
2020-05-13 11:23:46 -0500

    🐛  BUG: Split script back up, hopefully to fix redirect issue

--- static/brainstorm/scripts.js
+++ static/brainstorm/scripts.js
@@ -20,6 +20,8 @@ const getMobileOperatingSystem = () => {
   return 'unknown'
 }
 
+const deviceOS = getMobileOperatingSystem()
+
 const getQueryVariable = (variable) => {
   var query = window.location.search.substring(1)
   var vars = query.split('&')
@@ -29,28 +31,27 @@ const getQueryVariable = (variable) => {
       return pair[1]
     }
   }
-  return false
+  return ''
 }
 
-const deviceOS = getMobileOperatingSystem()
+const prompt = getQueryVariable('prompt')
 
-if (deviceOS === 'Andriod' || deviceOS === 'iOS') {
-  const prompt = getQueryVariable('prompt')
+if (deviceOS === 'Andriod') {
+  const now = new Date().valueOf()
+  setTimeout(function () {
+    if (new Date().valueOf() - now > 100) return
+    window.location =
+      'https://play.google.com/store/apps/details?id=com.shbrainstorm'
+  }, 25)
+  window.location = `brainstorm://${prompt}`
+}
 
-  if (deviceOS === 'Andriod') {
-    const now = new Date().valueOf()
-    setTimeout(function () {
-      if (new Date().valueOf() - now > 100) return
-      window.location =
-        'https://play.google.com/store/apps/details?id=com.shbrainstorm'
-    }, 50)
-  } else {
-    const now = new Date().valueOf()
-    setTimeout(function () {
-      if (new Date().valueOf() - now > 100) return
-      window.location =
-        'https://apps.apple.com/us/app/brain-storm/id1508034751?ls=1'
-    }, 50)
-  }
-  window.location = prompt ? `brainstorm://${prompt}` : 'brainstorm://'
+if (deviceOS === 'iOS') {
+  const now = new Date().valueOf()
+  setTimeout(function () {
+    if (new Date().valueOf() - now > 100) return
+    window.location =
+      'https://apps.apple.com/us/app/brain-storm/id1508034751?ls=1'
+  }, 25)
+  window.location = `brainstorm://${prompt}`
 }
