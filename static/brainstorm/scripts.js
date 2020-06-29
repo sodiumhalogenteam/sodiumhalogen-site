4305011ee3e2b876a83841d30d5c41c38c585f55
Brantley <brantley@sodiumhalogen.com>
2020-06-29 15:07:29 -0500

    📦  NEW: turns off brainstorm scripts

--- static/brainstorm/scripts.js
+++ static/brainstorm/scripts.js
@@ -6,35 +6,35 @@
  * @returns {String}
  */
 
-const getMobileOperatingSystem = () => {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera
+// const getMobileOperatingSystem = () => {
+//   var userAgent = navigator.userAgent || navigator.vendor || window.opera
 
-  if (/windows phone/i.test(userAgent)) {
-    return 'Windows Phone'
-  } else if (/android/i.test(userAgent)) {
-    return 'Android'
-  } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    return 'iOS'
-  }
+//   if (/windows phone/i.test(userAgent)) {
+//     return 'Windows Phone'
+//   } else if (/android/i.test(userAgent)) {
+//     return 'Android'
+//   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+//     return 'iOS'
+//   }
 
-  return 'unknown'
-}
+//   return 'unknown'
+// }
 
-const deviceOS = getMobileOperatingSystem()
+// const deviceOS = getMobileOperatingSystem()
 
-const getQueryVariable = (variable) => {
-  var query = window.location.search.substring(1)
-  var vars = query.split('&')
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=')
-    if (pair[0] == variable) {
-      return pair[1]
-    }
-  }
-  return ''
-}
+// const getQueryVariable = (variable) => {
+//   var query = window.location.search.substring(1)
+//   var vars = query.split('&')
+//   for (var i = 0; i < vars.length; i++) {
+//     var pair = vars[i].split('=')
+//     if (pair[0] == variable) {
+//       return pair[1]
+//     }
+//   }
+//   return ''
+// }
 
-const prompt = getQueryVariable('prompt')
+// const prompt = getQueryVariable('prompt')
 
 // if (deviceOS === 'Andriod' || deviceOS === 'iOS') {
 //   window.location = `brainstorm://${prompt}`
